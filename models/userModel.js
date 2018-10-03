b2ff43bde1e18f5a2500858deac0cfaf9c02afdd
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-03 13:29:33 +0300

    Created schema for users

---
+++ models/userModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'User name required'
+  },
+  bestScore: { type: Number, min: 0, default: 0 }
+});
+
+module.exports = mongoose.model('Users', userSchema);
