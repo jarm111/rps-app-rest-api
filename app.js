dcac5d50d9714572d8baaa6fd2b78b7a2397a3ef
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-04 10:31:36 +0300

    Added db connection and routes to App

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const Users = require("./models/userModel");
+const routes = require('./routes/userRoutes');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+mongoose.connect('mongodb://localhost/RpsUsersdb');
+mongoose.connection.on(
+  'error',
+  console.error.bind(console, 'connection error:')
+);
+
+routes(app);
+
+app.listen(port, () => console.log(`Rps REST API listening on port ${port}!`));
