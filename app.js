11bbe9262dc4cb6c163150cfff4285ef491987b1
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-05 12:47:26 +0300

    Changed port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 const mongoose = require('mongoose');
 const User = require('./models/userModel');
 const bodyParser = require('body-parser');
