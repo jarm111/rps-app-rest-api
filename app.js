fa3df139a7bc7ba4aae719a3fb7e2d4689d0f8f0
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-12 14:27:17 +0300

    Added passport config to app, restructured order

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-require('dotenv').config();
-const port = 5000;
 const mongoose = require('mongoose');
-const User = require('./models/userModel');
 const bodyParser = require('body-parser');
-const routes = require('./routes/userRoutes');
 const cors = require('cors');
+const passport = require('passport');
+const GoogleTokenStrategy = require('passport-google-token').Strategy;
+const port = process.env.PORT || '5000';
+require('./models/userModel');
+const routes = require('./routes/userRoutes');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(passport.initialize());
 
 mongoose.connect(
   process.env.DB_CONNECTION,
   { useNewUrlParser: true }
 );
+
 mongoose.connection.on(
   'error',
   console.error.bind(console, 'connection error:')
 );
 
-app.use(bodyParser.urlencoded({ extended: true }));
+passport.use(
+  new GoogleTokenStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+      console.log(done);
+      ((err, user) => {
+        return done(err, user);
+      })();
+    }
+  )
+);
 
 routes(app);
 
