c7046f94da3467268963a2502734646d9949a61c
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-14 16:02:31 +0300

    Added findOneOrCreate static method for User Model

--- models/userModel.js
+++ models/userModel.js
@@ -8,4 +8,15 @@ const userSchema = new mongoose.Schema({
   bestScore: { type: Number, min: 0, default: 0 }
 });
 
+userSchema.statics.findOneOrCreate = function(condition, callback) {
+  const self = this;
+  self.findOne(condition, (err, result) => {
+    return result
+      ? callback(err, result)
+      : self.create(condition, (err, result) => {
+          return callback(err, result);
+        });
+  });
+};
+
 module.exports = mongoose.model('User', userSchema);
