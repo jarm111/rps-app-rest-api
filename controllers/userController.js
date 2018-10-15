684cb00ef652b50df2a8c005839ce691ac144c77
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-15 15:21:50 +0300

    Removed redundant controller methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,33 +2,6 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const jwt = require('jsonwebtoken');
 
-exports.createUser = (req, res) => {
-  new User(req.body).save((err, user) => {
-    if (err) res.send(err);
-    res.json(user);
-  });
-};
-
-exports.readUserByName = (req, res) => {
-  User.findOne({ name: req.params.name }, (err, user) => {
-    if (err) res.send(err);
-    res.json(user);
-  });
-};
-
-exports.updateUserBestScoreByName = (req, res) => {
-  const newBestScore = req.body.bestScore;
-  User.findOneAndUpdate(
-    { name: req.params.name },
-    { bestScore: newBestScore },
-    { new: true },
-    (err, user) => {
-      if (err) res.send(err);
-      res.json(user);
-    }
-  );
-};
-
 exports.getTokenAndBestScore = (req, res) => {
   const token = jwt.sign(
     {
