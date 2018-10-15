e467f36c14ebbb230c6199d98f7d1a572a530af6
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-15 09:46:25 +0300

    Added route for google auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,16 @@
-module.exports = app => {
-  const user = require('../controllers/userController');
+const passport = require('passport');
+const user = require('../controllers/userController');
 
+module.exports = app => {
   app.route('/users/').post(user.createUser);
   app
     .route('/users/:name')
     .get(user.readUserByName)
     .put(user.updateUserBestScoreByName);
+  app
+    .route('/auth/google/')
+    .get(
+      passport.authenticate('google-token', { session: false }),
+      (req, res) => res.send(req.user)
+    );
 };
