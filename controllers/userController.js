4472a5df05b988304220a298c29c6ff4e971b1ee
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-15 14:50:43 +0300

    Added updateUserBestScore method to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,7 +29,7 @@ exports.updateUserBestScoreByName = (req, res) => {
   );
 };
 
-exports.getTokenAndScore = (req, res) => {
+exports.getTokenAndBestScore = (req, res) => {
   const token = jwt.sign(
     {
       googleId: req.user.googleId
@@ -38,4 +38,41 @@ exports.getTokenAndScore = (req, res) => {
     { expiresIn: '1d', issuer: 'RPS-App' }
   );
   res.json({ token, bestScore: req.user.bestScore });
+};
+
+exports.authenticateUser = (req, res, next) => {
+  const errorMessage = 'authorization header must be form: Bearer token';
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) return res.send(errorMessage);
+  const parts = authHeader.split(' ');
+  if (parts.length !== 2) return res.send(errorMessage);
+  const scheme = parts[0];
+  const credentials = parts[1];
+  if (!/^Bearer$/i.test(scheme)) return res.send(errorMessage);
+  const token = credentials;
+  jwt.verify(
+    token,
+    process.env.SECRET,
+    { issuer: 'RPS-App' },
+    (err, decoded) => {
+      if (err) return res.send(err);
+      res.locals.googleId = decoded.googleId;
+      next();
+    }
+  );
+};
+
+exports.updateUserBestScore = (req, res) => {
+  const newBestScore = req.body.bestScore;
+  if (!newBestScore)
+    return res.send('bestScore: number is required in request body');
+  User.findOneAndUpdate(
+    { googleId: res.locals.googleId },
+    { bestScore: newBestScore },
+    { new: true },
+    err => {
+      if (err) return res.send(err);
+      res.send("Successfully updated user's best score to " + newBestScore);
+    }
+  );
 };
